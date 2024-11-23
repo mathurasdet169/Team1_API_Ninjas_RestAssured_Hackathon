6711eccb792f607a591994728dd2147b7f3e107f
nirmala228 <nirmala.rp2024@gmail.com>
2024-11-22 21:17:00 -0500

    Update JsonReader.java

--- src/test/java/com/api/utilities/JsonReader.java
+++ src/test/java/com/api/utilities/JsonReader.java
@@ -46,54 +46,29 @@ public class JsonReader {
     	}
     	
     	
-    	public String login_json_Reader(String filepath,String scenarioNode) throws IOException
-    	{
-    	ObjectMapper objectMapper = new ObjectMapper();
-            // Read the JSON file
-            JsonNode rootNode = objectMapper.readTree(new File(filepath));
-            // Access the "scenarioNode" data
-            JsonNode Scenario_Node = rootNode.path(scenarioNode);
-            JsonNode ReqBodyNode=Scenario_Node.path("ReqBody");
-          
-           String loginData = objectMapper.writeValueAsString(ReqBodyNode);
-           JsonNode ValidationData=Scenario_Node.path("ValidationData");
-           EXP_STATUSCODE=ValidationData.get("exp_code").asInt();
-           System.out.println(EXP_STATUSCODE);
-           
-           endpoint=ValidationData.get("endpoint").asText();
-           System.out.println(endpoint);
-   		return loginData;
-           }
+//    	public String login_json_Reader(String filepath,String scenarioNode) throws IOException
+//    	{
+//    	ObjectMapper objectMapper = new ObjectMapper();
+//            // Read the JSON file
+//            JsonNode rootNode = objectMapper.readTree(new File(filepath));
+//            // Access the "scenarioNode" data
+//            JsonNode Scenario_Node = rootNode.path(scenarioNode);
+//            JsonNode ReqBodyNode=Scenario_Node.path("ReqBody");
+//          
+//           String loginData = objectMapper.writeValueAsString(ReqBodyNode);
+//           JsonNode ValidationData=Scenario_Node.path("ValidationData");
+//           EXP_STATUSCODE=ValidationData.get("exp_code").asInt();
+//           System.out.println(EXP_STATUSCODE);
+//           
+//           endpoint=ValidationData.get("endpoint").asText();
+//           System.out.println(endpoint);
+//   		return loginData;
+//           }
     	
     	
     		
 
-	public  String batch_json_Reader(String filepath,String scenarioNode) throws IOException
-	{
-		ObjectMapper objectMapper = new ObjectMapper();
-		// Read the JSON file
-		JsonNode rootNode = objectMapper.readTree(new File(filepath));
-		// Access the "scenarioNode" data
-		JsonNode Scenario_Node = rootNode.path(scenarioNode);
-		String batchDataJsonString = objectMapper.writeValueAsString(Scenario_Node);
-		//            BatchPayload batchData = objectMapper.treeToValue(Scenario_Node, BatchPayload.class);
-		//            BatchPayload batchPayLoadObj = new BatchPayload();
 
-		//            batchPayLoadObj.setBatchDescription(batchData.getBatchDescription());
-		//            batchPayLoadObj.setBatchName(batchData.getBatchName());
-		//            batchPayLoadObj.setBatchNoOfClasses(batchData.getBatchNoOfClasses());
-		//            batchPayLoadObj.setBatchStatus(batchData.getBatchStatus());
-		//            batchPayLoadObj.setProgramId(batchData.getProgramId());
-		//            batchPayLoadObj.setProgramName(batchData.getProgramName());
-
-		//            Gson gson = new GsonBuilder().setPrettyPrinting().create();
-		//			 String JsonRequestBody = gson.toJson(batchData);
-		//				System.out.println(JsonRequestBody);
-		return batchDataJsonString;
-
-
-
-	}
 
 
 	public LoginPayload login_json_Reader(String filepath,String scenarioNode) throws IOException
