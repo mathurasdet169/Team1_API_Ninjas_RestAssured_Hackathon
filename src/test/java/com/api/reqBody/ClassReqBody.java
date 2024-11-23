6442123be256805b6f9c210e2b966b48de06d0be
saranyashanmugam2024 <saranyashanmugam2023@gmail.com>
2024-11-22 21:16:23 -0500

    get method

--- src/test/java/com/api/reqBody/ClassReqBody.java
+++ src/test/java/com/api/reqBody/ClassReqBody.java
@@ -1,5 +1,69 @@
 package com.api.reqBody;
 
-public class ClassReqBody {
 
-}
+import java.io.IOException;
+import com.api.payLoad.*;
+import com.api.utilities.CommonUtils;
+
+
+
+import io.restassured.response.Response;
+
+public class ClassReqBody extends CommonUtils {
+
+	 public static Response response;
+	  String classEndpoint = endpoint.getString("getAllClass");
+	   String invalidEndpoint = endpoint.getString("invalidGetAllClass");
+	   
+//Get all class	scenarios  
+//with auth
+	 public Response getAllClassesWithAuth() throws IOException {
+		    String token = LoginReqBody.getBearerToken();
+		    if (token == null || token.isEmpty()) {
+		        throw new IllegalStateException("User token has not been generated.");
+		    }
+		    response = request.header("Authorization", "Bearer " + token).get(classEndpoint );
+		    logResponse("--Get All Classes with Auth--", response);
+		    return response;
+		}
+//invalid endpoint
+		public Response getAllClassesWithInvalidEndpoint() throws IOException {
+		    String token = LoginReqBody.getBearerToken();
+		    if (token == null || token.isEmpty()) {
+		        throw new IllegalStateException("User token has not been generated.");
+		    }
+		    response = request.header("Authorization", "Bearer " + token).get(invalidEndpoint);
+		    logResponse("--Get All Classes with Invalid Endpoint--", response);
+		    return response;
+		}
+//invalid method
+		public Response getAllClassesWithInvalidMethod() throws IOException {
+		    String token = LoginReqBody.getBearerToken();
+		    if (token == null || token.isEmpty()) {
+		        throw new IllegalStateException("User token has not been generated.");
+		    }
+		    response = request.header("Authorization", "Bearer " + token).post(classEndpoint); 
+		    logResponse("--Get All Classes with Invalid Method--", response);
+		    return response;
+		}
+//no auth
+		public Response getAllClassesWithoutAuth() throws IOException {
+		    response = request.get(classEndpoint); // No Authorization header
+		    logResponse("--Get All Classes without Auth--", response);
+		    return response;
+		}
+
+	
+//Loging the response 
+	    private void logResponse(String context, Response response) {
+	        System.out.println(context + " Status Code: " + response.statusCode());
+	        response.prettyPrint();
+	    }
+	    
+
+
+	    
+	    
+	    
+	}
+
