0db221a021d9b5ee1a3a84d68ae9af86e490abed
saranyashanmugam2024 <saranyashanmugam2023@gmail.com>
2024-11-25 20:09:57 -0500

    update jsonreader

--- src/test/java/com/api/utilities/JsonReader.java
+++ src/test/java/com/api/utilities/JsonReader.java
@@ -1,6 +1,12 @@
 package com.api.utilities;
+import com.api.payLoad.ClassPayload;
+import com.api.reqBody.ClassReqBody;
 import com.fasterxml.jackson.databind.JsonNode;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
 import java.io.File;
 import java.io.IOException;
 public class JsonReader {
@@ -27,5 +33,123 @@ public class JsonReader {
            }
     	
     	
-    		
+    	public  String batch_json_Reader(String filepath,String scenarioNode) throws IOException
+    	{
+    	ObjectMapper objectMapper = new ObjectMapper();
+            // Read the JSON file
+            JsonNode rootNode = objectMapper.readTree(new File(filepath));
+            // Access the "scenarioNode" data
+            JsonNode Scenario_Node = rootNode.path(scenarioNode);
+            String batchDataJsonString = objectMapper.writeValueAsString(Scenario_Node);
+//            BatchPayload batchData = objectMapper.treeToValue(Scenario_Node, BatchPayload.class);
+//            BatchPayload batchPayLoadObj = new BatchPayload();
+            
+//            batchPayLoadObj.setBatchDescription(batchData.getBatchDescription());
+//            batchPayLoadObj.setBatchName(batchData.getBatchName());
+//            batchPayLoadObj.setBatchNoOfClasses(batchData.getBatchNoOfClasses());
+//            batchPayLoadObj.setBatchStatus(batchData.getBatchStatus());
+//            batchPayLoadObj.setProgramId(batchData.getProgramId());
+//            batchPayLoadObj.setProgramName(batchData.getProgramName());
+            
+//            Gson gson = new GsonBuilder().setPrettyPrinting().create();
+//			 String JsonRequestBody = gson.toJson(batchData);
+//				System.out.println(JsonRequestBody);
+				return batchDataJsonString;
+            
+	
+            
+    	}
+    	
+
+
+
+
+//import com.fasterxml.jackson.databind.JsonNode;
+//
+//import com.fasterxml.jackson.databind.ObjectMapper;
+//import com.api.payLoad.ClassPayload;
+//import com.fasterxml.jackson.core.JsonProcessingException;
+//import com.fasterxml.jackson.core.type.TypeReference;
+//
+//import java.io.File;
+//import java.io.IOException;
+//import java.util.List;
+//
+//public class JsonReader {
+//	
+//	//JsonReader jsonReaderObj = new JsonReader();
+//	public List<ClassPayload> getClassScenarios(String filepath){
+//		List<ClassPayload> scenarios = null;
+//		ObjectMapper objectMapper = new ObjectMapper();
+//		try {
+//			File jsonFile = new File(filepath);
+//			String jsonString=objectMapper.writeValueAsString(objectMapper.readTree(jsonFile));
+//			scenarios = objectMapper.readValue(jsonString, new TypeReference<List<ClassPayload>>(){});
+//		} catch (JsonProcessingException e) {
+//			e.printStackTrace();
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		}
+//		return scenarios;
+//	}
+//	public String getString(String string) {
+//		// TODO Auto-generated method stub
+//		return null;
+//	}
+
+	
+	
+	
+
+//
+//	public String classJsonReader(String filepath, String scenarioNode) throws IOException {
+//	    ObjectMapper objectMapper = new ObjectMapper();
+//	    // Read the JSON file
+//    JsonNode rootNode = objectMapper.readTree(new File(filepath));
+//       // Access the "scenarioNode" data
+//       JsonNode Scenario_Node = rootNode.path(scenarioNode);
+//        String classJsondatastring = objectMapper.writeValueAsString(Scenario_Node);
+//      return classJsondatastring ;
+//
+//	}
+//	
+//	
+	
+	
+	public  String classJsonReader(String filepath,String scenarioNode) throws IOException
+	{
+	ObjectMapper objectMapper = new ObjectMapper();
+        // Read the JSON file
+        JsonNode rootNode = objectMapper.readTree(new File(filepath));
+        // Access the "scenarioNode" data
+        JsonNode Scenario_Node = rootNode.path(scenarioNode);
+        ClassPayload classreq = objectMapper.treeToValue(Scenario_Node, ClassPayload.class);
+//        JsonNode addressnode=Scenario_Node.path("userAddress");
+//        AddressDetails address=objectMapper.treeToValue(addressnode, AddressDetails.class);
+//
+        ClassPayload classobj=new ClassPayload();
+	//	AddressDetails addressDetailsObj=new AddressDetails();
+		
+        classobj.setBatchId(ClassReqBody.batchId);
+        classobj.setClassComments(classreq.getClassComments());
+        classobj.setClassDate(classreq.getClassDate());
+        classobj.setClassDescription(classreq.getClassDescription());
+        classobj.setClassNo(classreq.getClassNo());
+        classobj.setClassNotes(classreq.getClassNotes());
+        classobj.setClassRecordingPath(classreq.getClassRecordingPath());
+        classobj.setClassStaffId(classreq.getClassStaffId());
+        classobj.setClassStatus(classreq.getClassStatus());
+        classobj.setClassTopic(classreq.getClassTopic());
+        classobj.setClassScheduledDates(classreq.getClassScheduledDates());
+		
+		Gson gson = new GsonBuilder().setPrettyPrinting().create();
+		 String JsonRequestBody = gson.toJson(classobj);
+			//System.out.println(JsonRequestBody);
+			return JsonRequestBody;
+			
+	}
+	
+	
+	
+	
 }
